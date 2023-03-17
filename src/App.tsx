e7e0a3c69d03ac158e6cbe08912d0aadc9b41ded
KrzysztofKopec <kkopec119@gmail.com>
2023-03-17 16:26:57 +0100

    V2 update

--- src/App.tsx
+++ src/App.tsx
@@ -10,55 +10,65 @@ import {
   Flex,
   Center,
 } from "@chakra-ui/react";
-// import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import { Logo } from "./components/logo/Logo";
-import { GradientLogo } from "./components/logo/GradientLogo";
-import Gallery from "./components/gallery/Gallery";
 import "./styles.css";
 
 export const App = () => (
   // {"bg":"#FDF4F1","font":"#36231D","icon-gradient-0":"#EB6E00","icon-gradient-1":"#F3BCE6","slogan":"#4a3731"}
 
   <ChakraProvider theme={theme}>
-    <Box fontSize="xl" p={5} backgroundColor={"#FDF4F1"} minH="100vh">
-      {/* <ColorModeSwitcher justifySelf="flex-end" /> */}
+    <Box fontSize="xl" p={5} backgroundColor={"#FFFFFF"} minH="100vh">
       <HStack justifyContent={"space-between"}>
         <Box>
           <Logo width={"100%"} height={"100%"} />
         </Box>
-        <HStack gap={50} paddingRight={5}>
-          <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
-            Strona Główna
-          </Text>
-          <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
-            Fotoksiążka
-          </Text>
-          <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
-            O Nas
-          </Text>
-          <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
-            Kontakt
-          </Text>
-        </HStack>
+        {true ? (
+          <HStack gap={50} paddingRight={5}>
+            <Text bgColor={"#F69608"} bgClip="text">
+              Strona Główna
+            </Text>
+            <Text bgColor={"#F69608"} bgClip="text">
+              Fotoksiążka
+            </Text>
+            <Text bgColor={"#F69608"} bgClip="text">
+              O Nas
+            </Text>
+            <Text bgColor={"#F69608"} bgClip="text">
+              Kontakt
+            </Text>
+          </HStack>
+        ) : (
+          <HStack gap={50} paddingRight={5}>
+            <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
+              Strona Główna
+            </Text>
+            <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
+              Fotoksiążka
+            </Text>
+            <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
+              O Nas
+            </Text>
+            <Text bgGradient="linear(to-l, #ee8bd7, #EB6E00)" bgClip="text">
+              Kontakt
+            </Text>
+          </HStack>
+        )}
       </HStack>
-      <Divider marginTop={3} orientation="horizontal" borderColor={"#36231D"} />
-      {/* <Logo /> */}
-      {/* <GradientLogo /> */}
-      {/* <Center width={"60%"} paddingTop={50} paddingBottom={100}>
-        <Gallery />
-      </Center> */}
-      {/* <Center width={"100%"} paddingTop={50} paddingBottom={100}>
-        <Center width={"60%"}>
-          <Gallery />
-        </Center>
-      </Center> */}
-      {/* <Center width={"100%"} paddingTop={50} paddingBottom={100}>
-        <Center width={"60%"}>
-          <div className="example-container">
-            <Gallery />
-          </div>
-        </Center>
-      </Center> */}
+      {true ? (
+        <Box
+          marginTop={3}
+          // p={0.5}
+          p={0.3}
+          borderRadius="md"
+          bgGradient="linear(to-r, #EB6E00, #ee8bd7)"
+        />
+      ) : (
+        <Divider
+          marginTop={3}
+          orientation="horizontal"
+          borderColor={"#36231D"}
+        />
+      )}
     </Box>
   </ChakraProvider>
 );
