f5f62ce70c5a51a9b8766c9adcd7ed09d20e530a
KrzysztofKopec <kkopec119@gmail.com>
2023-03-18 16:50:56 +0100

    V5 update

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {
   Flex,
   Center,
   Container,
+  VStack,
 } from "@chakra-ui/react";
 import { Logo } from "./components/logo/Logo";
 import { GradientLogo } from "./components/logo/GradientLogo";
@@ -18,54 +19,86 @@ import GradientButton from "./components/buttons/GradientButton";
 import OneColorText from "./components/text/OneColorText";
 import Background from "../src/assets/backgroundImages/bg1.png"; //1-5
 const showButtons = false;
-const stickyHeader = true;
 
 export const App = () => (
   // {"bg":"#FDF4F1","font":"#36231D","icon-gradient-0":"#EB6E00","icon-gradient-1":"#d865bd","slogan":"#4a3731"}
+  // background: rgb(255,217,246);
+  // background: linear-gradient(153deg, rgba(255,217,246,1) 0%, rgba(245,218,218,1) 53%, rgba(255,199,142,0.9920343137254902) 100%);
 
   <ChakraProvider theme={theme}>
     <Box
       fontSize="xl"
       p={5}
-      backgroundImage={`url(${Background})`}
+      // backgroundImage={`url(${Background})`}
       height="full"
       width={"full"}
-      backdropBlur={"5px"}
-      backdropFilter={"auto"}
+      bgGradient="linear(to-br, rgba(255,217,246,1) 0%, rgba(245,218,218,1) 53%, rgba(255,199,142,0.9920343137254902)100%)"
     >
       <Center width={"100%"}>
-        <HStack
-          justifyContent={"space-between"}
+        <VStack
+          width={"80%"}
           backdropFilter={"auto"}
-          backdropBlur={"5px"}
+          backdropBlur={"55px"}
           borderRadius="15px"
           boxShadow={"dark-lg"}
-          position={stickyHeader ? "fixed" : "initial"}
-          marginTop={stickyHeader ? "100px" : "0px"}
-          width={stickyHeader ? "95%" : "100%"}
-          zIndex={stickyHeader ? 1 : 0}
         >
-          <Box>
-            <Logo width={"100%"} height={"100%"} />
-          </Box>
-          {showButtons ? (
-            <HStack gap={50} paddingRight={5}>
-              <GradientButton>Strona Główna</GradientButton>
-              <GradientButton>Fotoksiążka</GradientButton>
-              <GradientButton>O Nas</GradientButton>
-              <GradientButton>Kontakt</GradientButton>
-            </HStack>
-          ) : (
-            <HStack gap={50} paddingRight={55}>
-              <OneColorText>Strona Główna</OneColorText>
-              <OneColorText>Fotoksiążka</OneColorText>
-              <OneColorText>O Nas</OneColorText>
-              <OneColorText>Kontakt</OneColorText>
-            </HStack>
-          )}
-        </HStack>
+          <HStack justifyContent={"space-between"} width={"100%"}>
+            <Box>
+              <Logo width={"100%"} height={"100%"} />
+            </Box>
+            {showButtons ? (
+              <HStack gap={50} paddingRight={5}>
+                <GradientButton>Strona Główna</GradientButton>
+                <GradientButton>Fotoksiążka</GradientButton>
+                <GradientButton>O Nas</GradientButton>
+                <GradientButton>Kontakt</GradientButton>
+              </HStack>
+            ) : (
+              <HStack gap={50} paddingRight={55}>
+                <OneColorText>Strona Główna</OneColorText>
+                <OneColorText>Fotoksiążka</OneColorText>
+                <OneColorText>O Nas</OneColorText>
+                <OneColorText>Kontakt</OneColorText>
+              </HStack>
+            )}
+          </HStack>
+          <Center width={"100%"}>
+            <Box width={"60%"}>
+              <GradientLogo />
+            </Box>
+          </Center>
+          <Container>
+            <Text>
+              Lorem ipsum dolor sit amet. Non alias commodi nam vitae inventore
+              et laboriosam reiciendis et autem nobis sed facilis atque eum
+              illum quia quo optio molestiae? In tempora quis sit molestiae
+              saepe qui ducimus aliquam. Qui totam unde qui optio nesciunt ut
+              dolores unde.
+            </Text>
+            <Text paddingTop={10} paddingBottom={10}>
+              Est quas dignissimos ut sapiente quia ab fuga provident eum autem
+              sint non tempore officia. Sed autem dolor vel reprehenderit fuga
+              qui vero quasi id voluptate ipsam aut veniam consequatur est
+              suscipit optio nam aliquam magnam. Est iure eveniet vel aperiam
+              repudiandae in assumenda nihil quo excepturi nihil aut quibusdam
+              expedita aut optio dignissimos. Qui odit tempore qui recusandae
+              optio in explicabo nemo eos nulla accusamus est galisum facilis ea
+              sint odio aut dignissimos beatae.
+            </Text>
+            <Text paddingBottom={10}>
+              Est quas dignissimos ut sapiente quia ab fuga provident eum autem
+              sint non tempore officia. Sed autem dolor vel reprehenderit fuga
+              qui vero quasi id voluptate ipsam aut veniam consequatur est
+              suscipit optio nam aliquam magnam. Est iure eveniet vel aperiam
+              repudiandae in assumenda nihil quo excepturi nihil aut quibusdam
+              expedita aut optio dignissimos. Qui odit tempore qui recusandae
+              optio in explicabo nemo eos nulla accusamus est galisum facilis ea
+              sint odio aut dignissimos beatae.
+            </Text>
+          </Container>
+        </VStack>
       </Center>
-      <Center paddingTop={stickyHeader ? 150 : 10}>
+      <Center paddingTop={10}>
         <Box
           borderRadius="15px"
           width={"80%"}
