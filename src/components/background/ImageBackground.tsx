e0f00b2fbb729e056cb2caa34b1931ea377bb6f9
KrzysztofKopec <kkopec119@gmail.com>
2023-03-20 15:19:13 +0100

    V5.0 update

--- src/components/background/ImageBackground.tsx
+++ src/components/background/ImageBackground.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ImageBackground: React.FC<Props> = ({ imageURL, children }) => {
   return (
     <Flex minHeight="100vh">
-      <Box flex="2" bgColor={"#fff6ef"} width="80vw">
+      <Box flex="2" bgColor={"#fff6ef"} width="80vw" borderRight={"2px"}>
         {children}
       </Box>
       <Box
