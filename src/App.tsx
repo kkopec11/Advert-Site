a9aebf94cc993e754a47e5c5da4920f4c75ef969
KrzysztofKopec <kkopec119@gmail.com>
2023-05-03 17:39:16 +0200

    New Fonts added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import InitialView from "./views/InitialView/InitialView";
 import LowerView from "./views/LowerView/LowerView";
+import "@fontsource/inter";
+import "@fontsource/raleway";
+import "@fontsource/carlito";
+
+const theme = extendTheme({
+  // fonts: {
+  //   heading: `'Inter'`,
+  //   body: `'Inter'`,
+  // },
+  // fonts: {
+  //   heading: `'Carlito'`,
+  //   body: `'Carlito'`,
+  // },
+  // fonts: {
+  //   heading: `'Raleway'`,
+  //   body: `'Raleway'`,
+  // },
+});
 
 export const App = () => (
   <ChakraProvider theme={theme}>
